f99c753577a99ad25da2b59fa47c89ae5c7e7a84
Guo <dominic@Guos-Mac-mini.lan>
2022-10-06 21:14:09 -0400

    first commit back end

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const cors = require('cors')
+const knex = require('knex')
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: '',
+        password: '',
+        database: 'inotes'
+    }
+})  // to connect the server to the postgresql database
+/* 
+db.select('*').from('inotes').then(data => {
+    console.log(data);
+}) */
+
+const app = express()
+app.use(express.json())  //to parse json
+app.use(cors())  //to allow access to from front end to the server
+
+app.get('/notes', (req, res) => {
+    db('inotes').orderBy('id', 'asc').then(data => {
+        res.status(200).json(data)
+    })
+
+})
+
+app.get('/note/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('inotes').where('id', '=', id).then(data => {
+        res.status(200).json(data)
+    })
+})
+
+app.put('/update/:id', (req, res) => {
+    const { body, updated } = req.body;
+    db('inotes').where('id', req.params.id)
+        .update({
+            'body': body,
+            'updated': updated
+        })
+        .then(res.status(200).send('recieved'))
+        .catch(err => res.status(400).json('unable to update'))
+
+})
+
+app.delete('/delete/:id', (req, res) => {
+    db('inotes').where('id', req.params.id)
+        .del()
+        .then(res.json({ success: true }))
+        .catch(err => res.status(400).json('unable to delete'))
+}
+)
+
+app.post('/note/new', (req, res) => {
+    const { body, updated } = req.body;
+    db('inotes')
+        .insert({
+            'body': body,
+            'updated': updated
+        })
+        .then(res.status(200).send('recieved'))
+        .catch(err => res.status(400).json('unable to create'))
+})
+
+
+app.listen(3000, () => {
+    console.log('app is running ok')
+});
